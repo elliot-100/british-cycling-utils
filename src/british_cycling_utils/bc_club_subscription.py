b1c94f8cce277bf462bc7083469b5cf4d61c8b2d
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2025-06-29 18:17:20 +0100

    Changed: re-implement with attrs + cattrs instead of pydantic

---
+++ src/british_cycling_utils/bc_club_subscription.py
@@ -0,0 +1,86 @@
+"""Module containing `BCPersonRecord` class and associated code."""
+
+import csv
+from collections.abc import Mapping
+from datetime import UTC, datetime
+from pathlib import Path
+from typing import Any, Self
+
+from attrs import define
+from cattrs import register_structure_hook, structure
+
+
+def _convert_bc_date(value: str, type_: datetime) -> datetime | None:  # noqa: ARG001
+    """Converts from string in BC data to datetime or None."""
+    return datetime.strptime(value, "%d/%m/%Y").astimezone(UTC) if value else None
+
+
+@define(kw_only=True, frozen=True)
+class BcClubSubscription:
+    """Maps directly to a TODO record in the BC Club Management Tool."""
+
+    # Other column names: dob, emergency_contact_name, emergency_contact_number,
+    # primary_club, membership_type, membership_status, valid_to_dt, age_category,
+    # Address 1, Address 2, Address 3, Address 4, Address 5, Address 6, Country,
+    # Road & Track Licence Cat
+
+    first_name: str
+    """Required, appears always populated in CSV.
+    CSV column: same name.
+    BC UI column: 'Forename'."""
+
+    last_name: str
+    """Required, appears always populated in CSV.
+    CSV column: same name.
+    BC UI column: 'Surname'."""
+
+    email: str
+    """Required, appears always populated in CSV.
+    CSV column: same name.
+    BC UI column: 'Email'."""
+
+    telephone_day: str
+    """Required, appears always populated in CSV.
+    CSV column: same name
+    BC UI column: 'Telephone'."""
+
+    bc_membership_number: int
+    """Required, appears always populated in CSV.
+    This is a really a BC profile/login id, not limited to current BC members.
+    CSV column: 'membership_number'.
+    BC UI column: 'British Cycling Member', but blank if not a current BC member."""
+
+    expiry: datetime | None
+    """Optional, observed not always populated in CSV.
+    CSV column: 'end_dt'.
+    BC UI column: 'Club Membership Expiry'."""
+
+    @classmethod
+    def from_bc_data(cls, bc_data: Mapping[str, Any]) -> Self:
+        """Create instance from BC data.
+        Ignores non-implemented fields.
+        Aliases and converts fields
+        """
+        register_structure_hook(datetime, _convert_bc_date)
+        return structure(
+            {
+                "first_name": bc_data["first_name"],
+                "last_name": bc_data["last_name"],
+                "email": bc_data["email"],
+                "telephone_day": bc_data["telephone_day"],
+                "bc_membership_number": bc_data["membership_number"],
+                "expiry": bc_data["end_dt"],
+            },
+            cls,
+        )
+
+    @classmethod
+    def list_from_bc_csv(cls, file_path: Path) -> list[Self]:
+        """Take a CSV export from the BC system and return a list of instances."""
+        if not Path(file_path).is_file():
+            err_msg = f"`{file_path}`."
+            raise FileNotFoundError(err_msg)
+
+        with file_path.open(newline="") as csv_file:
+            reader = csv.DictReader(csv_file)
+            return [cls.from_bc_data(row) for row in reader]
---
+++ tests/test_bc_club_subscription.py
@@ -0,0 +1,41 @@
+"""Tests for 'BC' functions."""
+
+from datetime import UTC, datetime
+
+from src.british_cycling_utils.bc_club_subscription import BcClubSubscription
+
+bc_data = {
+    "first_name": "Julia",
+    "last_name": "Roberts",
+    "membership_number": "12345",
+    "email": "julia@example.com",
+    "end_dt": "19/12/2024",
+    "telephone_day": "+441234567890",
+}
+
+bc_data_with_blank = {
+    "first_name": "Kevin",
+    "last_name": "Bacon",
+    "membership_number": "54321",
+    "email": "kevin@example.com",
+    "end_dt": "",
+    "telephone_day": "+441234567890",
+}
+
+
+def test_from_bc_data__happy_path() -> None:
+    """Test that a BCPersonRecord is returned from BC data."""
+    sub = BcClubSubscription.from_bc_data(bc_data)
+    assert sub.email == "julia@example.com"
+    assert sub.first_name == "Julia"
+    assert sub.last_name == "Roberts"
+    assert sub.telephone_day == "+441234567890"
+    assert sub.bc_membership_number == 12345
+    assert sub.expiry
+    assert sub.expiry.date() == datetime(2024, 12, 19).astimezone(UTC).date()
+
+
+def test_from_bc_data__blank_fields() -> None:
+    """Test that a BCPersonRecord is returned from BC data when fields are blank."""
+    sub = BcClubSubscription.from_bc_data(bc_data_with_blank)
+    assert sub.expiry is None
